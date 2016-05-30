14eb445817b933ac052616ad3a9e1d6b2f82a8db
Adam Raider <adamjraider@gmail.com>
2016-05-30 10:21:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Starter Kit
+==============
+A web development project structure powered gulp and browser-sync.
+
+## Instructions
+
+```bash
+# Download project and unzip
+cd starter-kit
+npm install
+gulp serve
+```
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    watch = require('gulp-watch');
+
+var errorHandler = function (error) {
+  console.log(error.message);
+  this.emit("end");
+}
+
+gulp.task('styles', function() {
+  return gulp.src('./src/sass/application.sass')
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(sass({ indentedSyntax: true }).on('error', util.log))
+    .pipe(concat('application.css'))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.reload({ stream:true }))
+});
+
+gulp.task('views', function() {
+  return gulp.src('./src/views/**/*.pug')
+    .pipe(plumber())
+    .pipe(pug({ pretty: true }).on('error', util.log))
+    .pipe(gulp.dest('./app/'))
+    .pipe(browserSync.reload({ stream:true }))
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('./src/javascripts/**/*.js')
+    .pipe(plumber())
+    .pipe(concat("application.js"))
+    .pipe(gulp.dest('./app/javascripts'))
+    .pipe(rename("application.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./app/javascripts"))
+    .pipe(browserSync.reload({ stream:true }))
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./app/"
+    }
+  });
+  watch('./src/sass/**/*.sass', function(){ gulp.start('styles'); })
+  watch('./src/views/**/*.pug', function(){ gulp.start('views'); })
+  watch('./src/javascripts/**/*.js', function(){ gulp.start('scripts'); })
+  watch('./src/img/**/*', function(){ gulp.start('compress_images'); })
+});
